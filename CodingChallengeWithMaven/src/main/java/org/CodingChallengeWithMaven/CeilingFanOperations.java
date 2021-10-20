c38770b2f828da7739e7303ffffb2c3f80452374
Samyuktha0505 <33900725+Samyuktha0505@users.noreply.github.com>
2021-10-20 12:43:51 -0400

    Update CeilingFanOperations.java

--- CodingChallengeWithMaven/src/main/java/org/CodingChallengeWithMaven/CeilingFanOperations.java
+++ CodingChallengeWithMaven/src/main/java/org/CodingChallengeWithMaven/CeilingFanOperations.java
@@ -2,40 +2,31 @@ package org.CodingChallengeWithMaven;
 
 public class CeilingFanOperations {
 
-    private int currentFanSpeed;
-    private boolean reverseFlag;
+	private int currentFanSpeed;
+	private boolean reverseFlag;
 
-    public String getDirection() {
-        String direction = getDirectionFormat();
-        return direction;
-    }
+	public String getDirection() {
+		String direction = getDirectionFormat();
+		return direction;
+	}
 
-    private String getDirectionFormat() {
-        return reverseFlag ? "antiClockwise" : "clockWise";
-    }
+	private String getDirectionFormat() {
+		return reverseFlag ? "antiClockwise" : "clockWise";
+	}
 
-    public int changeSpeed(int currentFanSpeed) {
+	public int changeSpeed(int currentFanSpeed) {
 
-        if (currentFanSpeed == 3) {
-        	currentFanSpeed = 0;
-            return currentFanSpeed;
-        }
+		return (currentFanSpeed==3)?0:++currentFanSpeed;
+	}
 
-        return ++currentFanSpeed;
-    }
-
-    public int changeDirection(int currentFanSpeed) {
-        if (currentFanSpeed == 0) {
-            throw new DirectionException("Can't change direction, when Fan is at rest");
-        }
-        reverseFlag = !reverseFlag;
-        
-        System.out.println("Fan is rotating in : " + getDirectionFormat() + " direction");
-        return --currentFanSpeed;
-    }
-
-    private boolean isFanAtRest() {
-        return currentFanSpeed == 0;
-    }
+	public int changeDirection(int currentFanSpeed) {
+		if (currentFanSpeed == 0) {
+			throw new DirectionException("Can't change direction, when Fan is at rest");
+		}
+		reverseFlag = !reverseFlag;
+		// will prefer to use loggers later
+		System.out.println("Fan is rotating in : " + getDirectionFormat() + " direction");
+		return --currentFanSpeed;
+	}
 
 }
