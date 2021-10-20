f6d6ed8cc29e78640d3d0ac33b04b45f81e34354
Samyuktha0505 <33900725+Samyuktha0505@users.noreply.github.com>
2021-10-20 12:39:08 -0400

    Update CeilingFanOperations.java

--- JavaCodingChallenge/src/ceilingfandesign/CeilingFanOperations.java
+++ JavaCodingChallenge/src/ceilingfandesign/CeilingFanOperations.java
@@ -7,26 +7,15 @@ public class CeilingFanOperations {
 		currentfanspeed = 0;
 	}
 
+	// returns speed when chord is pulled in clockwise direction
 	public int pushForward(int speed) {
-		currentfanspeed= speed;
-		if(currentfanspeed==3) {
-			currentfanspeed=0;
-			return currentfanspeed;
-		}
-		else {
-			++currentfanspeed;
-			return currentfanspeed;
-		}
+		currentfanspeed= speed;			
+		return (currentfanspeed==3)?0:++currentfanspeed;		
 	}
-
+	
+	// returns speed when chord is pulled in anticlockwise direction
 	public int pushBackward(int speed) {
 		currentfanspeed= speed;
-		if(currentfanspeed==0) {
-			return currentfanspeed;
-		}
-		else {
-			--currentfanspeed;
-			return currentfanspeed;
-		}
+		return (currentfanspeed==0)?0:--currentfanspeed;
 	}
 }
